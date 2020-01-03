3cb123d6b45875ae10ab6648ef98fb4dca707c75
Jon Carroll <jon.carroll@epandcompany.com>
2020-01-03 12:03:48 -0500

    added type restrictions for FileResponse content

--- src/file-response.ts
+++ src/file-response.ts
@@ -1,9 +1,11 @@
+import { Stream } from "stream";
+
 export class FileResponse {
   public content: object;
   public fileName: string;
   public contentType: string;
 
-  constructor(content: object, fileName: string, contentType: string) {
+  constructor(content: Stream | Buffer, fileName: string, contentType: string) {
     this.content = content;
     this.fileName = fileName;
     this.contentType = contentType;
