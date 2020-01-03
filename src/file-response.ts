71f3776190f2d10fde83b966cd316c0880c34714
Jon Carroll <jon.carroll@epandcompany.com>
2020-01-03 11:24:23 -0500

    Updating file response with args

--- src/file-response.ts
+++ src/file-response.ts
@@ -1,8 +1,11 @@
 export class FileResponse {
   public content: object;
+  public fileName: string;
+  public contentType: string;
 
-  constructor(content: object) {
+  constructor(content: object, fileName: string, contentType: string) {
     this.content = content;
-    
+    this.fileName = fileName;
+    this.contentType = contentType;
   }
 }
